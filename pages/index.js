9d9a99f3d870da1632703e024ec8217a4870772e
Ronaldo Lima <4389565+ronal2do@users.noreply.github.com>
2019-06-29 16:04:53 +0200

    first

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import Head from 'next/head'
+
+var isMobile = {
+  Android: function() {
+      return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+      return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+      return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+      return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+  },
+  any: function() {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+  }
+};
+
+export default class extends React.Component {
+  timeoutId;
+
+  static async getInitialProps({ req }) {
+    console.log('@@', req.url)
+    // const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+    return { url: req.url };
+  }
+
+  async componentDidMount() {
+    const { url } = this.props;
+    console.log('this.props', url)
+    if ( isMobile ) {
+      // return window.location = `reciclabc://auth/reset/${url.token}`;
+      window.location = `exp://127.0.0.1:19000/--/auth/reset/${url.query.token}`;
+      this.timeoutId = setTimeout(() => {
+        window.location = 'https://reciclabc.com.br'
+      }, 300)
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timeoutId != null) {
+      clearTimeout(this.timeoutId)
+    }
+  }
+
+  render() {
+    return (
+      <div/>
+    )
+  }
+}
